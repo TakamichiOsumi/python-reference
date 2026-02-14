fcdf3a9bce2d88d7a8f0e8854fec932525ebec0e
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-02-14 13:32:09 +0900

    tutorial

---
+++ B/Card_Game_for_Two.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+N = int(input())
+ary = list(map(int, input().split()))
+
+ary.sort(reverse = True)
+
+turn = 0
+Alice = 0
+Bob = 0
+
+while len(ary) > 0:
+    if turn % 2 == 0:
+        Alice = Alice + ary.pop(0)
+    if turn % 2 == 1:
+        Bob = Bob + ary.pop(0)
+    turn = turn + 1
+
+print(Alice - Bob)
