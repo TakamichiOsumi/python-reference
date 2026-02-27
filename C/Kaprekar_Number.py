b70a7505e47981456475f5e538b00d46578abc47
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-02-28 06:04:03 +0900

    Kaprekar_Number.py

--- C/Kaprekar_Number.py
+++ C/Kaprekar_Number.py
@@ -5,10 +5,9 @@ N, K = map(int, input().split())
 x = N
 for k in range(K):
 
-    g1 = sorted(list(str(x)), reverse = True)
-    g2 = sorted(list(str(x)), reverse = False)
+    g1 = int(''.join(sorted(list(str(x)), reverse = True)))
+    g2 = int(''.join(sorted(list(str(x)), reverse = False)))
     # print("g1 =", g1, ", g2 =", g2)
-
-    x = int("".join(g1)) - int("".join(g2))
+    x = g1 - g2
 
 print(x)
