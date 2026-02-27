b131b2edbc5b3c4f786b6359f3831adddba52507
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-02-27 21:51:10 +0900

    Add comments to clarify the code logic.

--- B/Visibility.py
+++ B/Visibility.py
@@ -2,7 +2,7 @@
 
 H, W, X, Y = map(int, input().split())
 
-# This looks a twisted, misleading explanation.
+# This looks a twisted, misleading swap.
 START_Y = X - 1
 START_X = Y - 1
 
@@ -13,40 +13,40 @@ for i in range(H):
 
 visible = 1
 
-# Go up
+# Go up.
 y = START_Y
 x = START_X
-while y > 0:
+while y > 0: # y should be greater or equal to 1 to be decremented.
     if matrix[y - 1][x] == '.':
         visible += 1
         y -= 1
         continue
     break
 
-# Go down
+# Go down. Reset the position.
 y = START_Y
 x = START_X
-while y < H - 1:
+while y < H - 1: # y should be smaller than the last index to be incremented up to the bottom.
     if matrix[y + 1][x] == '.':
         visible += 1
         y += 1
         continue
     break
 
-# Go right
+# Go right. Reset the position.
 y = START_Y
 x = START_X
-while x < W - 1:
+while x < W - 1: # x should be smaller than the last index to be incremented to reach the rightmost cell.
     if matrix[y][x + 1] == '.':
         visible += 1
         x += 1
         continue
     break
 
-# Go left
+# Go left. Reset the position.
 y = START_Y
 x = START_X
-while x > 0:
+while x > 0: # x should be greater or equal to 1 to be decremented.
     if matrix[y][x - 1] == '.':
         visible += 1
         x -= 1
