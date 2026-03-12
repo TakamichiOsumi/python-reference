79b36a4ff4522e969a4cd3a75c20126b4cf61c2c
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-03-12 18:33:30 +0900

    Walking Takahashi

---
+++ C/Walking_Takahashi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+X, K, D = map(int, input().split())
+
+if X > 0:
+    if X - (K * D) > 0:
+        print(X - (K * D))
+    elif X  - (K * D) < 0:
+        used = (X // D) + 1
+        if (K - used) % 2 == 0:
+            print(abs(X - used * D))
+        else:
+            print(abs(X - (used * D) + D))
+    else:
+        # X - (K * D) == 0:
+        print(0)
+elif X < 0:
+    if X + (K * D) < 0:
+        print(abs(X + (K * D)))
+    elif X + (K * D) > 0:
+        used = (abs(X) // D) + 1
+        if (K - used) % 2 == 0:
+            print(abs(X + used * D))
+        else:
+            print(abs(X + (used * D) - D))
+    else:
+        # X - (K * D) == 0:
+        print(0)
+else:
+    # X == 0
+    if K % 2 == 0:
+        print(0)
+    else:
+        print(D)
