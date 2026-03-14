0e7351c29b1966572a96d525a85c1aefa7ff468d
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-03-14 23:14:55 +0900

    Deconstruct Chokolate

---
+++ B/Deconstruct_Chokolate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+H, W, Q = map(int, input().split())
+
+for i in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        R = query[1]
+        print(R * W)
+        H = H - R        
+    elif query[0] == 2:
+        C = query[1]
+        print(C * H)
+        W = W - C
