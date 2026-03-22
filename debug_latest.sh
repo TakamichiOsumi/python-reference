8d104cc322fdad141bcaadc140ccad7b74b121d1
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-03-23 07:48:16 +0900

    Minor fix of util files.

--- debug_latest.sh
+++ debug_latest.sh
@@ -1,3 +1,3 @@
-
 # Execute the last edited file and switch to the interactive debug mode if necessary.
+
 python3 -m pdb -c continue `ls -lt | head -2 | tail -1 | awk '{ print $9 }'`
--- pbcopy_latest.sh
+++ pbcopy_latest.sh
@@ -1,3 +1,3 @@
-
 # Copy the contents of the last edited file to clipboard.
+
 cat `ls -lt | head -2 | tail -1 | awk '{ print $9 }'` | pbcopy
