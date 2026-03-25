c3d0b86db6fa3b6fe728ce939dee79d5213be48e
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-03-25 12:42:37 +0900

    Let setup_dir.sh create input files for each assignment.

--- setup_dir.sh
+++ setup_dir.sh
@@ -7,8 +7,13 @@ else
     mkdir $1
     cd $1
     cp ../template.py A.py
+    touch A_{1..3}.txt
     cp ../template.py B.py
+    touch B_{1..3}.txt
     cp ../template.py C.py
+    touch C_{1..3}.txt
     cp ../template.py D.py
+    touch D_{1..3}.txt
     cp ../template.py E.py
+    touch E_{1..3}.txt
 fi
