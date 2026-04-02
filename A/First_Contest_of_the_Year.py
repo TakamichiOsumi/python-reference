335e4d3482c3b1b15b0099f83c2100c8695af548
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-04-02 20:42:02 +0900

    Added some minor takeaways.

--- A/First_Contest_of_the_Year.py
+++ A/First_Contest_of_the_Year.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-# Key Takeaways : Read the example of the problem carefully.
+# Key Takeaways :
+#
+# 1. Read the example of the problem carefully.
+# 2. Create an corner case by myself and evaluate the executed code.
+#    In this example, suppose the case F == D - 1. 6 days are left
+#    and taken over to the next year, which leads to the 6th day
+#    for the 1st contest.
 
 D, F = map(int, input().split())
 
