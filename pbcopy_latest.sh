3922a36db926a1235ed8f6b94bafb1e8cd375f61
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-04-04 18:51:11 +0900

    Updated pbcopy_latest.sh to optionally trigger an creation of backup file.

--- pbcopy_latest.sh
+++ pbcopy_latest.sh
@@ -1,3 +1,13 @@
 # Copy the contents of the last edited file in the current directory to clipboard.
 
-cat `ls -lt | head -2 | tail -1 | awk '{ print $9 }'` | grep -vE "^[ \t]*#.*" | pbcopy
+latest_file=`ls -lt | head -2 | tail -1 | awk '{ print $9 }'`
+cat $latest_file | grep -vE "^[ \t]*#.*" | pbcopy
+
+# Trigger the creation of the backup file, if -c option is appended.
+while getopts "c" opt; do
+    case "$opt" in
+	c)
+	    cp $latest_file ${latest_file}.backup
+	    ;;
+    esac
+done
