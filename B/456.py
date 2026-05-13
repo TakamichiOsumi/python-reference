b5121188c1735f270068666a347a96736415e91b
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-13 22:26:23 +0900

    '456'

---
+++ B/456.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import itertools
+
+dices = [ list(map(int, input().split())) for _ in range(3) ]
+
+patterns = list(itertools.product(dices[0], dices[1], dices[2]))
+
+counts = 0
+for p in patterns:
+    l = list(p)
+    if l.count(4) == 1 and l.count(5) and l.count(6):
+        counts += 1
+
+print((counts) / len(list(patterns)))
