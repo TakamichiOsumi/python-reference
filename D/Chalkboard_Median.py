ad20dcf183468bb154bc1dcda23977509008b6ea
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-17 09:03:05 +0900

    Minor comment cleanup.

--- D/Chalkboard_Median.py
+++ D/Chalkboard_Median.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 
-# import pdb
-
 from sortedcontainers import SortedList
-# from collections import deque
-# import itertools
-# import numpy
-# import re
 
 X = int(input())
 Q = int(input())
@@ -18,6 +12,5 @@ for i in range(Q):
     A, B = map(int, input().split())
     sl.add(A)
     sl.add(B)
-    # print("cur_status =",sl)
     print(sl[center_idx])
     center_idx += 1
