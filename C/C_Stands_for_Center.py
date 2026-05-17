b7fe07f04c209f3cc2fad5b0c94cdd116c652956
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-17 09:01:52 +0900

    Minor comment cleanup.

--- C/C_Stands_for_Center.py
+++ C/C_Stands_for_Center.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-# import pdb
-
-# from sortedcontainers import SortedList
-# from collections import deque
-# import itertools
-# import numpy
 import re
 
 S = input()
@@ -13,11 +7,8 @@ S = input()
 count = 0
 
 for m in re.finditer(r"C", S):
-    # print("Index=", m.start(0), "&", m.end(0), ", Sub string : ", S[ m.start(0) : m.end(0) ])
-
     center_C = m.start(0)
     close_edge = min(center_C, (len(S) - 1) - center_C)
     count += (close_edge + 1)
-    # print("close_edge=", close_edge)
 
 print(count)
