ee7ca8d9c874cdeefd7603300825373e60147600
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-17 09:04:55 +0900

    Minor shell script cleanup.

--- F/execute_latest.sh
+++
@@ -1,6 +0,0 @@
-# Execute the last edited file in the current directory.
-#
-# This is just a wrapper script to run any scripts.
-latest_file=`ls -lt | head -2 | tail -1 | awk '{ print $9 }'`
-
-python3 $latest_file
