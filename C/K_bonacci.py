239075791b575638718845863c90445168e4cad3
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-20 22:27:59 +0900

    Minor comment update of 'K_bonacci'

--- C/K_bonacci.py
+++ C/K_bonacci.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+#
+# (1) list.append() is an operation of O(1).
+#
+# (2) The current sum can be calculated by an idea to
+#     computing the sum of one specific reagion surrounded by
+#     ary[i - k] ~ ary[i] when i starts from the index = K.
+#     Removing ary[i - k] and adding ary[i] means shifting
+#     this reagion from left to right, one by one.
+
 N, K = map(int, input().split())
 
 cur_sum = K
