a750c9b74532ff561c02c1bfdf367e0818ffe4b7
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-20 21:36:20 +0900

    Updated some minor codes of '004'

--- 90/004.py
+++ 90/004.py
@@ -4,17 +4,21 @@
 #
 # (1) In this problem, applying T to numpy.array([]) was
 #     slower than just computing the columns_sum array.
+# (2) Sum of each column can be obtained by executing
+#     list(map(sum, zip(*matrix))).
 
 H, W = map(int, input().split())
 matrix = [ list(map(int, input().split())) for _ in range(H) ]
 rows_sum = [ sum(matrix[i]) for i in range(H) ]
 
-columns_sum = []
-for j in range(W):
-    total = 0
-    for i in range(H):
-        total += matrix[i][j]
-    columns_sum.append(total)
+# columns_sum = []
+# for j in range(W):
+#     total = 0
+#     for i in range(H):
+#         total += matrix[i][j]
+#     columns_sum.append(total)
+
+columns_sum = list(map(sum, zip(*matrix)))
 
 for i in range(H):
     for j in range(W):
