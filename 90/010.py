fe3751999c9cdd7d50e9b41268de299023342001
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-21 18:16:39 +0900

    Minor comment update of '010'

--- 90/010.py
+++ 90/010.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+
+# Key Takeaways :
+#
+# (1) This is a typical problem of cumulative sum.
+#     But, the way to retrieve the cumulative sum is not straightforward,
+#     if the index search is conducted in a simple manner.
+#     Padding zero to keep the indexes same between class1 and class2 is
+#     the good solution for this.
+
 import itertools
 
 N = int(input())
@@ -22,8 +31,6 @@ class2_accum = list(itertools.accumulate(class2))
 Query = int(input())
 for i in range(Query):
     L, R = map(int, input().split())
-
-    
     print(class1_accum[R] - class1_accum[L - 1], end="")
     print(" ", end="")
     print(class2_accum[R] - class2_accum[L - 1])
