14593ac48669f0e72a3105c27268a4416a999600
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-22 21:36:24 +0900

    '024'

---
+++ 90/024.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+N, K = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ary = [ abs(A[i] - B[i]) for i in range(N) ]
+
+diff = sum(ary)
+left = K - diff
+
+if left < 0:
+    print("No")
+else:
+    if left % 2 == 0:
+        print("Yes")
+    else:
+        print("No")
