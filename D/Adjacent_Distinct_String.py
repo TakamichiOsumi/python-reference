ae9fbc40dd2dbfdea84ddfa71d314013bc36f924
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-25 20:06:45 +0900

    Refined 'Adjacent Distinct String' to make it easier to write.

--- D/Adjacent_Distinct_String.py
+++ D/Adjacent_Distinct_String.py
@@ -40,13 +40,6 @@ for _ in range(T):
         # from the 1st (not the 0th !) index when putting characters
         # reach the end of array.
         result = [None] * final_len
-        last_idx = 0
-        for i in range(max_freq):
-            result[i * 2] = max_freq_key
-            last_idx = i * 2
-        # Move to and point to the next insert position.
-        last_idx += 2
-        counts.pop(max_freq_key, None)
 
         # Use deque as date structure.
         #
@@ -54,19 +47,17 @@ for _ in range(T):
         # elements are required, to pick up the next character to
         # insert. While appending to the end costs O(1), popping
         # from the leftmost element costs O(n) with list type.
-        cur_unsed_chars = deque([])
-        for k, v in counts.items():
-            cur_unsed_chars.extend([k] * v)
+        cur_unused_chars = deque([])
+        for key, val in sorted(counts.items(), key = lambda x:x[1]):
+            cur_unused_chars.extend([key] * val)
 
-        while cur_unsed_chars:
-            # Restart from the result[1] position if insert
-            # position exceeds the index of 'result' array.
+        last_idx = 0
+        while cur_unused_chars:
+            result[last_idx] = cur_unused_chars.pop()
+            last_idx += 2
+            # Restart from the left side.
             if final_len % 2 == 0 and last_idx == final_len:
                 last_idx = 1
             elif final_len % 2 == 1 and last_idx == final_len + 1:
                 last_idx = 1
-            
-            result[last_idx] = cur_unsed_chars.pop()
-            last_idx += 2
-
         print("".join(result))
