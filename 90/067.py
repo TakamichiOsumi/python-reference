30c635db8c25ce15824c334a2522edd5dad3b948
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-26 12:35:33 +0900

    Added some minor comments on '067.py'.

--- 90/067.py
+++ 90/067.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+
+# Get used to np.base_repr(N, M) and int(N, M).
+# String 'N' is interpreted as M-adic, when 'N' is passed to int(N, M)
+# np.base_repr(N, M) converts N to a number of 'M' base representation.
+
 import numpy as np
 
 N, K = input().split()
