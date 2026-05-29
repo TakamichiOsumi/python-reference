9282d66acd442180cf25554630e3d0f12146446a
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-29 11:42:05 +0900

    '403 Forbidden'

---
+++ C/403_Forbidden.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from sortedcontainers import SortedSet
+
+N, M, Q= map(int, input().split())
+
+user_view = [ SortedSet([]) for i in range(N) ]
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        user_view[query[1] - 1].add(query[2])
+    elif query[0] == 2:
+        user_view[query[1] - 1].add(0) # All !
+    elif query[0] == 3:
+        if 0 in user_view[query[1] - 1]:
+            print("Yes")
+        else:
+            if query[2] in user_view[query[1] - 1]:
+                print("Yes")
+            else:
+                print("No")
