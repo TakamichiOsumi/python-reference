fcc3ffb73aff8069e29826a7ba99b6356dc2d11c
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-05-29 11:56:57 +0900

    Minor refactoring of '403 Forbidden'

--- C/403_Forbidden.py
+++ C/403_Forbidden.py
@@ -8,15 +8,16 @@ user_view = [ SortedSet([]) for i in range(N) ]
 
 for _ in range(Q):
     query = list(map(int, input().split()))
+    uidx = query[1] - 1
     if query[0] == 1:
-        user_view[query[1] - 1].add(query[2])
+        user_view[uidx].add(query[2])
     elif query[0] == 2:
-        user_view[query[1] - 1].add(0) # All !
+        user_view[uidx].add(0) # All !
     elif query[0] == 3:
-        if 0 in user_view[query[1] - 1]:
+        if 0 in user_view[uidx]:
             print("Yes")
         else:
-            if query[2] in user_view[query[1] - 1]:
+            if query[2] in user_view[uidx]:
                 print("Yes")
             else:
                 print("No")
