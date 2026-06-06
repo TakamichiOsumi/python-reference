d5a19257a17c3278d54c06ad1137950fa013a703
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-06-06 09:52:07 +0900

    'Not All'

---
+++ B/Not_All.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from collections import deque
+
+N, M = map(int, input().split())
+A = deque(list(map(int, input().split())))
+
+cnt = 0
+required = set(list(range(1, M + 1)))
+while True:
+    set_a = set(A)
+    common = set_a & required
+    if len(common) < len(required):
+       break 
+    cnt += 1
+    A.pop()
+
+print(cnt)
