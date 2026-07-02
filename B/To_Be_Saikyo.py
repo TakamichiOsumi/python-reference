88997f3ff7b3617aceb2fb98497d2bf24a366877
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-07-02 21:13:41 +0900

    Added comments on 'To Be Saikyo'.

--- B/To_Be_Saikyo.py
+++ B/To_Be_Saikyo.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# Key Takeawasy:
+#
+# (1) N can be 1.
+# (2) max(-2, -3, -4, -5, -6) returns -2.
+#     If 'diffs' are all minus value, it means the 1st person is already strongest.
+#     Yet, max(-2, -3, -4, -5, -6) + 1 returns -1 and this is a wrong answer.
+#     The answer should be 0 if the 1st person is strong enough from the beginning.
+#     This check can be expressed as max(diffs) < 0.
+
 N = int(input())
 P = list(map(int, input().split()))
 
