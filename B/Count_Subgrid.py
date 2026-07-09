acf56a661c0bc9c0f3779b91035d459f3cbe074b
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-07-09 20:55:29 +0900

    Added comments on 'Count Subgrid'.

--- B/Count_Subgrid.py
+++ B/Count_Subgrid.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+#
+# (1) Nesting a smaller matrix in another matrix
+#     can be done based on an idea that the size
+#     of M - 1 cannot be used at the right and
+#     the bottom edge. Then, loop times is
+#     'N - (M - 1)'. The -1 is the used cell in
+#     the inner matrix.
+#
+# (2) Tuple can't be updated since it's immutable.
+#     Apply set() to tuple converted from list to
+#     filter unique combinations.
+
 N, M = map(int, input().split())
 
 S = [ list(input()) for _ in range(N) ]
