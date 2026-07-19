c93f47548c355459422cb857ef73408ab125b371
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-07-19 21:28:05 +0900

    Added comments on 'Obesity'.

--- A/Obesity.py
+++ A/Obesity.py
@@ -1,4 +1,14 @@
 
+# Key Takeaways:
+#
+# (1) Detect the calculation part that can generate float value
+#     and avoid the part which leads to float type.
+#
+#     In this example, the core part is "W / H / H".
+#     Therefore, "W / H / H >= (25 * 100 * 100)" cannot
+#     achieve AC and "W * 100 * 100 / H / H" is the expression
+#     to avoid calculation errors.
+
 H, W = map(int, input().split())
 
 if (W * 100 * 100 / (H ** 2)) >= 25.0:
