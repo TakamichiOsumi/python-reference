63817a80ac5e2547385bc4c0061fb6613e337265
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-07-30 15:55:40 +0900

    '1122 Substring 2'

---
+++ C/1122_Substring_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# from sortedcontainers import SortedList, SortedDict, SortedSet
+# from collections import deque
+# import itertools
+# import numpy
+import re
+
+S = input()
+
+splited = []
+# Split the entire string when it encounters a different character.
+for m in re.finditer(r"1+|2+|3+|4+|5+|6+|7+|8+|9+|0+", S):
+    splited.append(S[ m.start(0) : m.end(0) ])
+
+ans = 0
+for i in range(len(splited) - 1):
+    if int(splited[i][0]) + 1 == int(splited[i + 1][0]):
+        ans += min(len(splited[i]),
+                   len(splited[i + 1]))
+
+print(ans)
+
