5425650dd9cad0c5c9e81c0e41b35bff2e1acbeb
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-08-17 14:24:19 +0900

    Added comments on 'Chargers'.

--- re/D/Chargers.py
+++ re/D/Chargers.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+#
+# Adding a new element by O(logN) is acceptable for query = 1.
+#
+# The main point for this problem is *how to avoid updating each
+# battery charged values based on elapsed time*.
+#
+# This can be skipped when all battery values are converted to
+# battery values where the time is equal to 0 and saved in a
+# sorted list. Then, printing the biggest value with the time
+# of query = 2.
+
 from sortedcontainers import SortedList, SortedDict, SortedSet
 
 Q, V = map(int, input().split())
