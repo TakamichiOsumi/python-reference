6cdf2b4a0138b237de594e506117536fc8a9d323
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-08-17 13:14:22 +0900

    Minor cleanup of codes.

--- A/Nine_or_Nein.py
+++ A/Nine_or_Nein.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-# from sortedcontainers import SortedList, SortedDict, SortedSet
-# from collections import deque, Counter
-# import itertools
-# import numpy
-# import re
-
 A, B = map(int, input().split())
-
 
 if A + B == 9 or A - B == 9 or A * B == 9 or A / B == 9:
     print("Nine")
--- B/Survey_Tabulation.py
+++ B/Survey_Tabulation.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
-# from sortedcontainers import SortedList, SortedDict, SortedSet
 from collections import deque, Counter
-# import itertools
-# import numpy
-# import re
-
-debug_mode = False
-def p(*var):
-    global debug_mode
-    if debug_mode:
-        print("DEBUG:", *var)
 
 N = int(input())
 strings = [ input().lower() for _ in range(N) ]
