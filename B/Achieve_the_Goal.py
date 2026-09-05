fe421fc3189ccb0e53054ce895da70768cbd0883
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-09-05 21:47:51 +0900

    Fixed 'Achieve the Goal' for better readability.

--- B/Achieve_the_Goal.py
+++ B/Achieve_the_Goal.py
@@ -3,11 +3,8 @@
 N, K, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-if N * M - sum(A) > K:
+val = N * M - sum(A)
+if val > K:
     print(-1)
 else:
-    if N * M - sum(A) <= 0:
-        print(0)
-    else:
-        print(N * M - sum(A))
-    
+    print(max(0, val))
