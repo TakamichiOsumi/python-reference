f149372f84d8ab2170bd5c54c72f45a3665a94d1
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-09-05 21:44:29 +0900

    'Achieve the Goal'

---
+++ B/Achieve_the_Goal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+N, K, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+if N * M - sum(A) > K:
+    print(-1)
+else:
+    if N * M - sum(A) <= 0:
+        print(0)
+    else:
+        print(N * M - sum(A))
+    
