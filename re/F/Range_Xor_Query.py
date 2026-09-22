178e4c9daae626aa80f9174726d9f2a0277f8f38
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-09-23 08:54:10 +0900

    Comments addition to 'Range Xor Query'.

--- re/F/Range_Xor_Query.py
+++ re/F/Range_Xor_Query.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+#
+# When building a segment tree, note that input array
+# doesn't get updated, only by calling st.set().
+# It updates only the internal array. Therefore,
+# use st.get() for the value reference like [1].
+
 from atcoder.segtree import SegTree
 
 def xor_op(d1, d2):
@@ -13,7 +20,7 @@ st = SegTree(xor_op, 0, A)
 for _ in range(Q):
     T, X, Y = map(int, input().split())
     if T == 1:
-        ref = st.get(X - 1)
+        ref = st.get(X - 1) # ... [1]
         st.set(X - 1, ref ^ Y)
     else:
         val = st.prod(X - 1, Y)
