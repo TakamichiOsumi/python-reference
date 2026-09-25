221ddd0330545fb3ea1c0986af7e298b09edfc6b
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-09-25 14:51:51 +0900

    Added some comments on '038' under 90 dir.

--- 90/038.py
+++ 90/038.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+# Key Takeaways:
+#
+# Each answer can be calculated by
+# (the total number until the 'r'th day) - (the total number until the 'l - 1'th day).
+#
+# Padding 0 to the leftmost index by insert() enables simple reference to accum[r] and
+# diff calculation using accum[l - 1] with accum[r].
+#
+# This logic with 0 is easy to understand, when listing two examples as below.
+# (1) The value from the 1st day to Nth day can be calculated by accum[N] (- accum[0]),
+#     where accum[0] is zero.
+# (2) The value from the 2nd day to Nth day can be calculated by accum[N] - accum[1].
+
 import itertools
 
 N, Q = map(int, input().split())
