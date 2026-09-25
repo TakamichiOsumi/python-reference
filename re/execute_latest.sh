627bc08e408972387fd6d356660d49735577b216
Takamichi Osumi <takamichiosumi01@gmail.com>
2026-09-25 15:21:12 +0900

    Created execute_latest.sh caller script for 're' dir.

---
+++ re/execute_latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Call another one-level above execute_latest.sh and
+# execute the last edited file in the current directory.
+#
+# This is just a wrapper script to run any scripts for under 're' dir.
+bash ../../execute_latest.sh
